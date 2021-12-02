9cb4517f4629c4aeb113a6aaa7c2d35e5bfc160a
Your Name <you@example.com>
2021-12-02 20:53:40 -0300

    Adding EF Core DBContext class for in memory database

---
+++ WebAPICarros.Domain/Model/CarDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPICarros.Domain.Model
+{
+    public class CarDbContext : DbContext
+    {
+        public CarDbContext(DbContextOptions<CarDbContext> options) : base(options)
+        { }
+
+        public DbSet<CarModel> CarModels { get; set; } = null!;
+    }
+}
