1950135e17ed75aaf5f58d4cdf311c200b768145
João Paulo <santanajps@taginfraestrutura.com.br>
2022-03-07 23:51:37 -0300

    repository generic

---
+++ WebApiCarros.Repository/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApiCarros.Repository
+{
+    public class BaseRepository
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        ObjectId Id { get; set; }
+    }
+}
---
+++ WebApiCarros.Repository/Interface/IMongoRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace WebApiCarros.Repository.Interface
+{
+  public interface IMongoRepository <TDocument> where TDocument : IBaseRepository 
+    {
+        Task<TDocument> FindByIdAsync(string id);
+        void InsertOne(TDocument document);
+        Task InsertOneAsync(TDocument document);
+        void InsertMany(ICollection<TDocument> documents);
+        Task InsertManyAsync(ICollection<TDocument> documents);
+        void ReplaceOne(TDocument document);
+        Task ReplaceOneAsync(TDocument document);
+        void DeleteById(string id);
+        Task DeleteByIdAsync(string id);
+
+        void DeleteMany(Expression<Func<TDocument, bool>>, Filter);
+
+
+    }
+}
